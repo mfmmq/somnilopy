298a70f9afeeebbe34b313d1d7ac0d5adfcda7bc
mfmmq <maya@mfmmq.xyz>
2019-04-12 20:26:35 +0100

    First working cut of front end

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import logging
 import argparse
 import os
 from somnilopy.somnilopy import Somnilopy
-
+from OpenSSL import SSL
+from datetime import time
 import wave
 import contextlib
 
@@ -50,21 +51,67 @@ if __name__ == "__main__":
     snippets_queue = []
     somnilopy = Somnilopy(args.schedule, args.force, args.dir, args.file_name)
     #somnilopy.run()
-    from flask import Flask, render_template, request, jsonify, Response
+    from flask import Flask, render_template, request, jsonify, Response, send_file
+    from flask_cors import CORS
     app = Flask(__name__)
+    CORS(app)
 
-    @app.route('/', methods = ['GET'])
-    def home():
+    @app.route('/files', methods = ['GET'])
+    def file_name():
+        all_file_names = os.listdir(args.dir)
+        all_file_names.sort(key=lambda x: os.path.getmtime("autosave/"+x))
+
+        file_info = []
+        for name in all_file_names:
+            date, time = name.split("_")[1:3]
+            time = time.replace(".wav","")
+            time = time.replace("-",":")[0:5]
+            path = os.path.join(args.dir, name)
+            with contextlib.closing(wave.open(path, 'r')) as f:
+                 frames = f.getnframes()
+                 rate = f.getframerate()
+                 length = round(frames / float(rate),2)
+            file_info.append({"date": date, "time": time, "length": length, "name": name})
+        # Create a list of info grouped by date ie file: [..] date: 04-09
+        files_by_date = []
+        for file in file_info:
+            if file['date'] not in [date_group['date'] for date_group in files_by_date]:
+                files_by_date.append({'date': file['date'], 'files': []})
+            for date_group in files_by_date:
+                if file['date'] == date_group['date']:
+                    date_group['files'].append(file)
+        logging.debug(f"Got file dates: {[date_group['date'] for date_group in files_by_date]}")
+        files_by_date.reverse()
+        return jsonify(files_by_date)
+
+    @app.route('/delete/<name>', methods = ['DELETE'])
+    def delete():
         all_file_names = os.listdir(args.dir)
         file_info = []
         for name in all_file_names:
-            day, time = name.split("_")[1:3]
-            with contextlib.closing(wave.open(audiofile, 'r')) as f:
-                frames = f.getnframes()
-                rate = f.getframerate()
-                length = frames / float(rate)
-                print(length)
-                file_info.append({"day": day, "time": time, "length": length})
+            date, time = name.split("_")[1:3]
+            time = time.replace(".wav","")
+            time = time.replace("-",":")
+            with contextlib.closing(wave.open(os.path.join(args.dir, name), 'r')) as f:
+                 frames = f.getnframes()
+                 rate = f.getframerate()
+                 length = frames / float(rate)
+            file_info.append({"date": date, "time": time, "length": length})
+        logging.info(file_info)
+        return jsonify(file_info)
 
-        return render_template("index.html", file_info=file_info)
+    @app.route('/download/<name>', methods=['GET'])
+    def download(name):
+        path = f"autosave/{name}"
+        logging.info(path)
+        return send_file(
+            path,
+            mimetype="x-please-download-audio/wav",
+            as_attachment=True,
+            attachment_filename=name)
+
+    @app.route('/not-sleeptalking/<name>', methods=['GET']):
+    def not_sleeptalking(name):
+        return None
+
     app.run()
--- static/sleeptalk.js
+++ static/sleeptalk.js
@@ -1,98 +1,252 @@
-var file_info = [
-	{"start_time": "00:31", "length": "90", "path": ""},
-	{"start_time": "04:31", "length": "360", "path": ""},
-	{"start_time": "02:01", "length": "40", "path": ""},
-	{"start_time": "00:51", "length": "20", "path": ""},
-	{"start_time": "05:55", "length": "200", "path": ""}
-];
+var DEFAULT_BACKGROUND_COLOR = "rgba(190, 190, 190, 0.3)";
+var DEFAULT_BORDER_COLOR = "rgb(120, 120, 120, 0.5)";
+var DOWNLOAD_URL = "download";
+var HOST = "http://127.0.0.1:5000";
 
-var file_info = {{file_info}}
+function loadPage() {
+  var request = new XMLHttpRequest();
+  request.open('GET', HOST+'/files');
+  request.send();
+  document.getElementById("message").innerHTML = "Could not load sleeptalking data. Is the server running?"
+  request.onreadystatechange=(e)=>{
+    // Do some initialising variables
+    var ctx = document.getElementById('myChart').getContext('2d');
+    // Get data
+    obj_files = JSON.parse(request.responseText);
+    unique_days = createDays(obj_files);
+    // Set fields
+    document.getElementById("message").innerHTML = ""
+    document.getElementById("datenav-name").innerHTML = "Date"
+    document.getElementById("timenav-name").innerHTML = "Time"
+    document.getElementById("info-name").innerHTML = "About"
 
-var recording_lengths = Array(file_info.length)
-for (i = 0; i < file_info.length; i++) {
-	recording_lengths[i] = file_info[i]["length"]
+    // Load chart
+    chart_data = createChartData(obj_files[0].files)
+    chart = createChart(ctx, chart_data);
+    // Load page
+    loadDateLinks(0);
+    loadTimeLinks(0, 0)
+    loadFileInfo(0,0); 
+  };
 }
 
-var time_labels = Array(file_info.length);
-for (i = 0; i < file_info.length; i++) {
-	time_labels[i] = file_info[i]["start_time"]
+function removeData(evt, myChart) {
+  var element = myChart.getElementAtEvent(evt);
+  if(element.length > 0) {
+    var ind = element[0]._index;
+    myChart.data.datasets[0].data.splice(ind, 1);
+    myChart.data.labels.splice(ind, 1);
+    myChart.update();
+  }
 }
 
-var barchartdata =  {
-        labels: time_labels,
-        datasets: [
-            {
-            label: 's',
-            data: recording_lengths,
-            borderWidth: 1
-        }]
-    };
-
-function load_file_info() {
-	var file_info_html = ""
-	for (i=0; i<file_info.length; i++) {
-		
-	}
-	document.getElementById('info').innerHTML = ""
-}
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+function createChart(ctx, barchartdata) {
+  var myChart = new Chart(ctx, {
     type: 'bar',
     data: barchartdata,
     defaultFontFamily: "'AlteHaasGrotesk','format/font/AlteHaasGrotesk.eot','format/font/AlteHaasGrotesk.woff','format/font/AlteHaasGrotesk.ttf'",
     options: {
-		legend: {
+      title: {
+        text: 'Chart'
+      },  
+      legend: {
 			display: false
 		},
-		onClick: function(evt) {   
-			var element = myChart.getElementAtEvent(evt);
-			if(element.length > 0) {
-				var ind = element[0]._index;
-				if(confirm('Do you want to remove this point?')){
-		            document.getElementById('info').innerHTML = "Hello";
-					data.datasets[0].data.splice(ind, 1);
-                    document.getElementById('info').innerHTML = "Hello1";
-					data.labels.splice(ind, 1);
-                    document.getElementById('info').innerHTML = "Hello2";
-					myChart.update(data);
-				}}
-          
-		},
-        tooltips: {
-          mode: 'index',
-          intersect: true,
-		  displayColors: false
-        },
-        scales: {
-            yAxes: [{
-				scaleLabel: {
-					display: true, 
-					labelString: "Length in Seconds"},                
-				ticks: {
-                    beginAtZero: true
-                },
-                stacked: true,
-				gridLines: {
-					display: true,
-					drawBorder: true,
-					drawOnChartArea: false
-				}
-            }],
-            xAxes: [{
-				scaleLabel: {
-					display:true,
-					labelString: "Time of Day"
-				},
-				stacked: true,
-				gridLines: {
-					display: true,
-					drawBorder: true,
-					drawOnChartArea: false
-				}
-			}
+      onHover: function(e) {
+        var point = this.getElementAtEvent(e);
+        if (point.length) e.target.style.cursor = 'pointer';
+        else e.target.style.cursor = 'default';
+      },
+      onClick: function(evt) {   
+          removeData(evt, myChart)
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: true,
+        displayColors: false
+      },
+      scales: {
+          yAxes: [{
+              scaleLabel: {
+                  display: true, 
+                  labelString: "Length in Seconds"},                
+              ticks: {
+                  beginAtZero: true
+              },
+              stacked: true,
+              gridLines: {
+                  display: true,
+                  drawBorder: true,
+                  drawOnChartArea: true
+              }
+          }],
+          xAxes: [{
+              scaleLabel: {
+                  display:true,
+                  labelString: "Time of Day"
+              },
+              stacked: true,
+              gridLines: {
+                  display: true,
+                  drawBorder: true,
+                  drawOnChartArea: true
+              }
+          }
           
           ]
         }
+      }
+  });
+  return myChart
+}
+
+function createChartData(day_obj_files) {
+  var start_times = [];
+  var lengths = [];
+  var background_color = [];
+  var border_color = [];
+  for (i=0; i<day_obj_files.length; i++) {
+    start_times.push(day_obj_files[i].time);
+    lengths.push(day_obj_files[i].length);
+    background_color.push(DEFAULT_BACKGROUND_COLOR);
+    border_color.push(DEFAULT_BORDER_COLOR);
+  };
+  var chart_data =  {
+        title: "my",
+        labels: start_times,
+        datasets: [
+            {
+            label: 'length',
+            data: lengths,
+            borderWidth: 1,
+            backgroundColor: background_color,
+            borderColor: border_color
+        }]
+    };
+  return chart_data
+}
+
+function createDays(obj_files) {
+  unique_days = []
+  for (i=0;i<obj_files.length;i++){
+    unique_days.push(obj_files[i].date)
+  }
+  // Sort these perhaps
+  return unique_days
+}
+
+function loadDateLinks(date_num) {
+  link_string = "<font style='text-decoration: italic'><i>"
+  for (i=0; i<unique_days.length; i++) {
+    a_href_class = "date-link"
+    if (i==date_num) {
+      a_href_class = " date-link active";
     }
-});
+    link_string = link_string.concat('<a href=# onClick="loadDateLinks('+i+')"  class="'+a_href_class+'">'+unique_days[i]+' </a>')
+  }
+  link_string = link_string.concat("</i></font>")
+  document.getElementById("datenav").innerHTML = link_string;
+  loadChartData(obj_files[date_num])
+  loadTimeLinks(date_num, 0)
+  
+}
+
+function loadChartData(day_obj_files) {
+  chart_data = createChartData(day_obj_files.files);
+  chart.data = chart_data;
+  chart.update()
+}
+
+function loadChartBarColor(date_num, time_num) {
+  for (i=0; i<obj_files[date_num].files.length; i++) {
+    if (i==time_num) {
+      chart.data.datasets[0].backgroundColor[i] = "teal";
+      chart.data.datasets[0].borderColor[i] = "teal";
+    }
+    else {
+      chart.data.datasets[0].backgroundColor[i] = DEFAULT_BACKGROUND_COLOR;
+      chart.data.datasets[0].borderColor[i] = DEFAULT_BORDER_COLOR;
+    }
+  }
+  chart.update()
+}
+
+
+function loadTimeLinks(date_num, time_num) {
+  files = obj_files[date_num].files
+  link_string = "<font style='text-decoration: italic'><i>"
+  for (j=0; j<files.length; j++) {
+    a_href_class = "time-link";
+    if (j == time_num) {
+      a_href_class += " time-link active";
+    }
+    link_string = link_string.concat('<a href=# onClick="loadTimeLinks('+date_num+', '+j+')" class="'+a_href_class+'">'+files[j].time+'</a> ')
+  }
+  link_string = link_string.concat("</i></font>")
+  document.getElementById("timenav").innerHTML = link_string;
+  loadFileInfo(date_num, time_num)
+  loadChartBarColor(date_num, time_num)
+}
+
+function loadFileInfo(date_num, time_num) {
+  // Write a little blurb
+  obj_file = obj_files[date_num].files[time_num]
+  info_html = "Name: " + obj_file.name + "<br>"
+  info_html += "Length: " + obj_file.length + " seconds<br>"
+  // Create some buttons
+  //play_button_html = '<button onClick="playSample('+date_num+','+time_num+')">Play sample</button> '
+  save_button_html = '<button onClick=saveSample("'+obj_file.name+'") id=saveSample>Save sample</button> '
+  //button_html = "<button onClick='deleteSample()'>Delete sample</button> "
+  button_html = "<button>Not sleeptalking</button> "
+  
+  document.getElementById('info').innerHTML = info_html
+  document.getElementById('actions').innerHTML =  save_button_html + button_html
+  
+}
+
+function playSample(date_num, time_num) {
+}
+
+function saveSample(file_name) {
+  // Download file with file_name 
+  path = HOST+'/download/'+file_name
+  download_request = new XMLHttpRequest();
+  download_request.responseType = 'blob';
+  download_request.open('GET', path);
+  download_request.send();
+  download_request.addEventListener('readystatechange', function(e) {
+	if(download_request.readyState == 2 && download_request.status == 200) {
+		// Download is being started
+	}
+	else if(download_request.readyState == 3) {
+		// Download is under progress
+	}
+	else if(download_request.readyState == 4) {
+      // Download file immediately
+      // https://stackoverflow.com/questions/13405129/javascript-create-and-save-file
+      var a = document.createElement("a"),
+      url = URL.createObjectURL(download_request.response);
+      a.href = url;
+      a.download = file_name;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function() {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);  
+        }, 0
+      ); 
+    }
+  })
+} 
+
+
+
+var obj_files = [];
+var chart = null;
+var myChart = null;
+loadPage()
+
+
+
+
+
