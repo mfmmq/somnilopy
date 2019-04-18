e91dd66ee31d764ac689ac84586c365ce78f4821
jbremz <jhbremner@gmail.com>
2019-04-18 17:11:56 +0100

    speech2text init

--- somnilopy/sleeptalk_processor.py
+++ somnilopy/sleeptalk_processor.py
@@ -7,27 +7,46 @@ import wave
 import os
 import time
 import pyaudio
+import speech_recognition as sr
 
 
 class SleeptalkProcessor:
-    def __init__(self, folder, file_prefix):
+    def __init__(self, folder, file_prefix, to_text=False):
         self.folder = folder
         self.file_prefix = file_prefix
         self.sample_width = pyaudio.get_sample_size(pyaudio.paInt16)
         self.loop = True
+        if to_text:
+            self.recognizer = sr.Recognizer()
 
     def process_snippets(self, snippets_queue, stop_event, default_sleep_time=2):
         sleep_time = default_sleep_time
         while self.loop and not stop_event.is_set():
             if len(snippets_queue):
                 snippet_tuple = snippets_queue.pop(0)
-                self.write_to_file(snippet_tuple)
+                file_path = self.write_to_file(snippet_tuple)
                 sleep_time = default_sleep_time
             else:
                 time.sleep(sleep_time)
         self.stop(snippets_queue)
 
-    def write_to_file(self, snippet_tuple):
+    def speech2text(self, file_path):
+        '''
+        Takes a path to an audio file and a recognizer object 
+        --> Applies speech recognition, returns result as string
+        '''
+        if not self.recognizer:
+            self.recognizer = sr.Recognizer()
+        try:
+            file = sr.AudioFile(file_path)
+        except AssertionError:
+            lgging.info(f"Invalid audio file type for speech2text")
+            return
+        with file as source:
+            audio = self.recognizer.record(source)
+        return self.recognizer.recognize_google(audio)
+
+    def write_to_file(self, snippet_tuple, to_text=False):
         snippet, timestamp = snippet_tuple
         file_name = f"{self.file_prefix}_{timestamp.strftime('%m-%d_%H-%M-%S')}.wav"
         file_path = os.path.join(self.folder, file_name)
@@ -39,6 +58,7 @@ class SleeptalkProcessor:
         wf.writeframes(snippet)
         wf.close()
         logging.info(f"Saved snippet at {file_path}")
+        return file_path
 
     def stop(self, snippets_queue):
         logging.info(f"Stopping SleeptalkProcessor, stop event set")
