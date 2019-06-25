835f7803383ebb972bde48033242fb9a04ccdad0
mfmmq <maya@mfmmq.xyz>
2019-06-25 20:59:05 +0100

    Do some code refactoring towards abstracting away file format

--- somnilopy/api.py
+++ somnilopy/api.py
@@ -1,18 +1,28 @@
 import logging
-import os
-import contextlib
-import wave
 from datetime import datetime
-from flask import Flask, jsonify, Response, send_file
+from flask import Flask, jsonify, Response, send_file, abort
 from flask_cors import CORS
+from flask_restful import Resource
 
+class BackendApi:
+    def __init__(self):
+        self.hello = 0
+        ns = api.namespace('blog/categories', description='Operations related to blog categories')
 
-def run_backend(autosave_dir, start_time, stop_time):
+        class FilesCollection(Resource)
+
+        @api.response(404, 'File not found.')
+        class FilesItem(Resource)
+
+def run_backend(start_time, stop_time, file_handler):
+    '''
+    Set up and run the back-end api for the frond-end that serves webpages and static files
+    :param start_time:
+    :param stop_time:
+    :return:
+    '''
     app = Flask(__name__)
     CORS(app)
-    prefix_dir = 'recordings'
-    is_sleeptalking_dir = os.path.join('recordings', 'is-sleeptalking')
-    not_sleeptalking_dir = os.path.join('recordings', 'not-sleeptalking')
 
     @app.route('/recording', methods=['GET'])
     def is_recording():
@@ -22,28 +32,13 @@ def run_backend(autosave_dir, start_time, stop_time):
             return None
 
     @app.route('/files', methods=['GET'])
-    def file_name():
-        all_file_paths = [os.path.join(autosave_dir, file_name) for file_name in os.listdir(autosave_dir)]
-        all_file_paths.extend([os.path.join(is_sleeptalking_dir, file_name) for file_name in os.listdir(is_sleeptalking_dir)])
-        all_file_paths.extend([os.path.join(not_sleeptalking_dir, file_name) for file_name in os.listdir(not_sleeptalking_dir)])
-        all_file_paths.sort(key=lambda x: os.path.getmtime(x))
-
+    def get_files_info():
+        file_paths = file_handler.get_all_sorted_file_paths()
         file_info = []
-        for file_path in all_file_paths:
-            try:
-                date, time = file_path.split("_")[1:3]
-                time = time.replace(".wav", "")
-                time = time.replace("-", ":")[0:5]
-                label, name = os.path.split(file_path)
-                label = os.path.split(label)[1]
-                with contextlib.closing(wave.open(file_path, 'r')) as f:
-                    frames = f.getnframes()
-                    rate = f.getframerate()
-                    length = round(frames / float(rate), 2)
-                file_info.append({"date": date, "time": time, "length": length, "name": name, "label": label})
-            except ValueError:
-                continue
+        for file_path in file_paths:
+            file_info.extend(file_handler.get_file_info_by_path(file_path))
         # Create a list of info grouped by date ie file: [..] date: 04-09
+
         files_by_date = []
         for file in file_info:
             if file['date'] not in [date_group['date'] for date_group in files_by_date]:
@@ -51,49 +46,55 @@ def run_backend(autosave_dir, start_time, stop_time):
             for date_group in files_by_date:
                 if file['date'] == date_group['date']:
                     date_group['files'].append(file)
-        logging.debug(f"Got file dates: {[date_group['date'] for date_group in files_by_date]}")
-        files_by_date.reverse()
+        logging.info(f"Got file dates: {[date_group['date'] for date_group in files_by_date]}")
         return jsonify(files_by_date)
 
-
-    @app.route('/delete/<label>/<name>', methods=['DELETE'])
+    @app.route('/files/<label>/<name>', methods=['DELETE'])
     def delete(label, name):
         # Make sure we have that file, else send a 404
-        try:
-            # Move to a not-sleeptalking folder
-            current_path = os.path.join(prefix_dir, label, name)
-            new_path = os.path.join(prefix_dir, 'delete', name)
-            logging.info(f"Moving {current_path} to {new_path}")
-            os.rename(current_path, new_path)
-            return Response("Successfully deleted file", mimetype='text/html', status=200)
-        except FileNotFoundError:
-            # Send a 404 reponse back
-            return Response("File not found", mimetype='text/html', status=404)
+        text, status = file_handler.delete(label, name)
+        return Response(text, mimetype='text/html', status=status)
 
-    @app.route('/download/<label>/<name>', methods=['GET'])
+    @app.route('/files/<label>/<name>', methods=['GET'])
     def download(label, name):
-        path = os.path.join('..', prefix_dir, label, name)
-        return send_file(
-            path,
-            mimetype="audio/wav",
-            as_attachment=True,
-            attachment_filename=name)
-
-    @app.route('/label/<new_label>/<old_label>/<name>', methods=['GET'])
-    def move_folder(old_label, new_label, name):
-        # Make sure we have that file, else send a 404
+        '''
+        Endpoint to download any sleeptalking file. Use the label to get the folder its located in
+        :param label:
+        :param name:
+        :return:
+        '''
+        path = file_handler.get_file_path_from_label(label, name)
         try:
-            old_label = os.path.join(prefix_dir, old_label)
-            new_label = os.path.join(prefix_dir, new_label)
-            # Move to label named folder
-            current_path = os.path.join(old_label, name)
-            new_path = os.path.join(new_label, name)
-            os.rename(current_path, new_path)
-            logging.info(current_path)
-            logging.info(new_path)
-            return Response("Successfully moved file", mimetype='text/html', status=200)
-        except FileNotFoundError:
-            # Send a 404 reponse back
-            return Response("File not found", mimetype='text/html', status=404)
+            return send_file(
+                path,
+                mimetype="audio/flac",
+                as_attachment=True,
+                attachment_filename=name)
+        except:
+            abort(500, 'Error getting file')
 
+    @app.route('/files/label/<old_label>/<name>/<new_label>', methods=['GET'])
+    def move_folder(old_label, new_label, name):
+        '''
+        Move a file from one folder to another. This acts as a pseudo persistent label without having to
+        modify comments or metadata. Having all the 'sleeptalking' and 'not sleeptalking' files in their
+        respective folders also makes it easier to train models
+        :param old_label:
+        :param new_label:
+        :param name:
+        :return:
+        '''
+        # Make sure we have that file, else send a 404
+        text, status = file_handler.apply_label(new_label, old_label, name)
+        return Response(text, mimetype='text/html', status=status)
+
+    @app.route('/files/comment/<label>/<name>', methods=['PUT'])
+    def update_comment(label, name):
+        if request.headers['Content-Type'] == 'text/plain':
+            file_handler.update_comment(label, name, request.data)
+        else:
+            return "415 Unsupported Media Type"
+        return Response('Successfully added comment', mimetype='text/html', status=200)
+
+    logging.info('Starting up api')
     app.run()
